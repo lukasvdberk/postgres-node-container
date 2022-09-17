089a88050c641d1121d3f2216e209957909db495
Lukas van den Berk <lukasvdberk@gmail.com>
2022-09-17 22:56:47 +0200

    initial postgres container model

--- src/models/running-postgres-container.model.ts
+++ src/models/running-postgres-container.model.ts
@@ -1,9 +1,29 @@
 import {StartedTestContainer} from "testcontainers";
 
 export class RunningPostgresContainerModel {
-    constructor(
-        private readonly container: StartedTestContainer
-    ) {}
+    private readonly container: StartedTestContainer
+    postgresHost: string
+    postgresUsername: string
+    postgresPassword: string
+    postgresPort: number
+    postgresDatabase: string
+
+
+    constructor(container: StartedTestContainer, postgresHost: string, postgresUsername: string, postgresPassword: string, postgresPort: number, postgresDatabase: string) {
+        this.container = container;
+        this.postgresHost = postgresHost;
+        this.postgresUsername = postgresUsername;
+        this.postgresPassword = postgresPassword;
+        this.postgresPort = postgresPort;
+        this.postgresDatabase = postgresDatabase;
+    }
+
+    /**
+     * Postgres' connection string you can use to connect to the container.
+     */
+    getPostgresConnectionString(): string {
+        return `postgres://${this.postgresUsername}:${this.postgresPassword}@${this.postgresDatabase}:${this.postgresPort}/${this.postgresDatabase}`
+    }
 
     /**
      * Stops the container and removes it.
